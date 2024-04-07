78ec4891ae751f91d56f74114f1c25b5782451a5
Klaus <klaus.cicd@outlook.com>
2024-04-07 22:40:18 +0800

    feat: 新增Prometheus监控服务demo

---
+++ demo-prometheus/docker/README.md
@@ -0,0 +1,6 @@
+# SpringCloud服务监控系统
+
+Prometheus+Loki+Grafana+Promtail 打造SpringCloud微服务监控面板
+
+> 参考文章 https://juejin.cn/post/7303741345322745908
+
---
+++ demo-prometheus/src/main/java/com/klaus/demo/prometheus/PrometheusApp.java
@@ -0,0 +1,23 @@
+package com.klaus.demo.prometheus;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Klaus
+ */
+@SpringBootApplication
+public class PrometheusApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusApp.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
