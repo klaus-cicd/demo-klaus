265690042ce48c54dde17a8d652d68bb93aa032d
Klaus <klaus.cicd@gmail.com>
2024-12-23 09:31:11 +0800

    docs(rabbit-mq): 延时消息补充相关注释

--- demo-rabbitmq/src/main/java/com/demo/rabbitmq/consumer/DelayMsgConsumer.java
+++ demo-rabbitmq/src/main/java/com/demo/rabbitmq/consumer/DelayMsgConsumer.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * 使用RabbitMQ的延时队列, 需要安装rabbitmq_delayed_message_exchange插件
+ *
  * @author Klaus
  */
 @Slf4j
@@ -41,14 +42,15 @@ public class DelayMsgConsumer {
 
     // 方式二: 注解式
     @RabbitListener(bindings = @QueueBinding(
+            // 踩坑点: 不能同时设置delayed=true和x-delayed-type="x-delayed-message", 否则无法创建交换机, 这两者都代表这是一个延时交换机
             exchange = @Exchange(value = MQConstant.DELAY_EXCHANGE, delayed = "true",
                     arguments = {@Argument(name = "x-delayed-type", value = "direct")}
             ),
             key = MQConstant.DELAY_MSG_TOPIC,
             value = @Queue(value = MQConstant.DELAY_MSG_QUEUE, arguments = {
-                    @Argument(name = "x-max-length", value = "100000", type = "java.lang.Integer")
+                    @Argument(name = "x-max-length", value = "300", type = "java.lang.Integer")
             })
-    ), ackMode = "AUTO")
+    ), ackMode = "AUTO")// 注意这里ACK模式是自动, 方法内别再手动ack了, 否则会导致MQ异常
     public void buzzerConsumer(Channel channel, Message message) {
         String msgBody = new String(message.getBody());
         log.info("收到延时消息, time: {}, message: {}", DateUtil.now(), msgBody);
